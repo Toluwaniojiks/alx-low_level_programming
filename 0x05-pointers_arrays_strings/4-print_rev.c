9a586c6757bf9539beac5838ace63a28cb3bcef0
Ojikutu Mary <maryojiks@yahoo.com>
2023-03-27 07:51:18 -0700

    function that prints a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *@s: pointer to the string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
