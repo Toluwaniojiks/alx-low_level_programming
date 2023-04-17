dba225a53f3e2d90ee4382140a9591d7f48fce2f
Ojikutu Mary <maryojiks@yahoo.com>
2023-04-17 07:21:49 -0700

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - struct(datatype) representing a dog
+ * @name: The dog's name 
+ * @age: The dog's age
+ * @owner: The dog's owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+void free_dog(dog_t *d);
+
+#endif
+
