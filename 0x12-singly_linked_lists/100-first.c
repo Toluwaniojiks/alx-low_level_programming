e8db92a1abf90b0e16049d6fac10a36154644146
Ojikutu Mary <maryojiks@yahoo.com>
2023-04-27 07:29:00 -0700

    function thats prints a message before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
-
+void first(void) __attribute__ ((constructor));
 /**
- * bmain - function excuted before main
+ * first - function excuted before main
  * Return: void.
+ *
  */
-void __attribute__((contructor)) bmain()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
