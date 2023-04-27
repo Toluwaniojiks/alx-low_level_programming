dabf7983afa53b16ca8244366400bc871b28ec70
Ojikutu Mary <maryojiks@yahoo.com>
2023-04-27 07:20:43 -0700

    function thts prints a message before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * bmain - function excuted before main
+ * Return: void.
+ */
+void __attribute__((contructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
